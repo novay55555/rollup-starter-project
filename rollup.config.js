5a129cd1b6e2c4c397205fdd3e514ef4b43b74d9
gunhawk <gunhawk998@gmail.com>
2018-09-06 19:34:50 +0800

    update rollup version

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,8 @@
 import babel from 'rollup-plugin-babel';
 import babelrc from 'babelrc-rollup';
 import istanbul from 'rollup-plugin-istanbul';
+import merge from 'deepmerge';
+import { uglify } from 'rollup-plugin-uglify';
 
 let pkg = require('./package.json');
 let external = Object.keys(pkg.dependencies);
@@ -15,21 +17,31 @@ if (process.env.BUILD !== 'production') {
   }));
 }
 
-export default {
-  entry: 'lib/index.js',
-  plugins: plugins,
-  external: external,
-  targets: [
-    {
-      dest: pkg.main,
-      format: 'umd',
-      moduleName: 'rollupStarterProject',
-      sourceMap: true
-    },
-    {
-      dest: pkg.module,
-      format: 'es',
-      sourceMap: true
-    }
-  ]
+const base = {
+  input: 'lib/index.js',
+  plugins,
+  external,
+  output: {
+    file: pkg.main,
+    format: 'umd',
+    name: 'rollupStarterProject',
+    sourcemap: true
+  }
+
 };
+
+export default [
+  base,
+  merge(base, {
+    output: {
+      file: pkg.module,
+      format: 'es',
+    }
+  }),
+  merge(base, {
+    plugins: plugins.concat(uglify()),
+    output: {
+      file: pkg.minify,
+    }
+  })
+];
